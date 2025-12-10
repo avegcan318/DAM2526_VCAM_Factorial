86dfea32bce875f91ca0240e41d5d0d258133c89
Antonio Vega <avegcan318@g.educaand.es>
2025-12-10 08:52:23 +0100

    commit B: Refactorización con función pedirNumero() y atributo estático Scanner

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -1,22 +1,25 @@
 package pruebaControlVersiones;
-
 import java.util.Scanner;
 
 public class Factorial {
 
+    // Atributo estático para manejar el flujo de entrada
+    private static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+        int num = pedirNumero();
 
-		System.out.print("Introduce un número entero mayor o igual que 0: ");
-		int num = sc.nextInt();
+        if (num < 0) {
+            System.out.println("ERROR: el número debe ser mayor o igual que 0.");
+        } else {
+            System.out.println("Has introducido el número: " + num);
+        }
 
-		if (num < 0) {
-			System.out.println("ERROR: el número debe ser mayor o igual que 0.");
-		} else {
-			System.out.println("Has introducido el número: " + num);
-		}
-
-		sc.close();
+        sc.close();
 	}
 
+	private static int pedirNumero() {
+	    System.out.print("Introduce un número entero mayor o igual que 0: ");
+	    return sc.nextInt();
+	}
 }
